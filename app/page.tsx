68e0808dd2f57ad98a3bbe093eb5734362178c24
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-29 13:49:19 -0700

    Add background to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="w-screen h-screen relative">
+      <div
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(/main-bg.webp" }}
+      ></div>
+    </main>
+  );
 }
