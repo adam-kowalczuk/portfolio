d8c2356658e33b4e1724b1af6d4fa9da4a5ec9d0
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-29 14:36:56 -0700

    Add trees and stars images, refactor link classNames

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className=" text-[50px] text-white font-semibold">
-            Make anything possible with{" "}
+            Make anything possible with
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
               Web Development
             </span>
           </h1>
@@ -21,31 +22,32 @@ export default function Home() {
             maxime aliquam error nobis odit laudantium. Maiores dicta delectus
             esse odit fugiat! Provident.
           </p>
-          <div className="flex-col md:flex-row hidden md:flex">
+          <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
               href="skills"
-              className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
               Learn More
             </Link>
             <Link
               href="projects"
-              className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              {/* <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" /> */}
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               Projects
             </Link>
             <Link
               href="contact"
-              className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              {/* <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" /> */}
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               Contact
             </Link>
           </div>
         </div>
       </div>
-      <div className="absolute bottom-10 z-[20] right-5 flex-col md:hidden">
+      {/* Mobile layout */}
+      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
         <Link
           href="skills"
           className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
@@ -75,6 +77,22 @@ export default function Home() {
         />
         <Image src="/cliff.webp" alt="cliff" width={480} height={480} />
       </div>
+      <div className="absolute bottom-0 z-[5] w-full h-auto">
+        <Image
+          src="/trees.webp"
+          alt="trees"
+          width={2000}
+          height={2000}
+          className="w-full h-full "
+        />
+      </div>
+      <Image
+        src="/stars.png"
+        alt="stars"
+        height={300}
+        width={300}
+        className="absolute top-10 left-0 z-[10]"
+      />
     </main>
   );
 }
