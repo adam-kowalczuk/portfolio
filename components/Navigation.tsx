a020305276088ba31bb7880a161f92227b932c4d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-02 16:17:17 -0700

    Add isRouting, isActive, and prevPath states, as well as corresponding useEffects

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,8 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Navigation = () => {
   const [isRouting, setIsRouting] = useState(false);
+  // Get current browser path
+  const path = usePathname();
+  const [isActive, setIsActive] = useState(path);
+  const [prevPath, setPrevPath] = useState("/");
+
+  useEffect(() => {
+    if (prevPath !== path) {
+      setIsRouting(true);
+    }
+  }, [path, prevPath]);
+
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeout = setTimeout(() => {
+        setIsRouting(false);
+      }, 1200);
+      return () => clearTimeout(timeout);
+    }
+  }, [isRouting]);
+
   return <div>Navigation</div>;
 };
 
