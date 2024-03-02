2abc8a9ef6af4cd88f6a2da293aa501ca8d0c81c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-02 16:27:22 -0700

    Add and style icons

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { NavLinks } from "@/constants";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -6,7 +8,6 @@ const Navigation = () => {
   const [isRouting, setIsRouting] = useState(false);
   // Get current browser path
   const path = usePathname();
-  const [isActive, setIsActive] = useState(path);
   const [prevPath, setPrevPath] = useState("/");
 
   useEffect(() => {
@@ -25,7 +26,22 @@ const Navigation = () => {
     }
   }, [isRouting]);
 
-  return <div>Navigation</div>;
+  return (
+    <div
+      style={{ left: "20%" }}
+      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white"
+    >
+      {NavLinks.map((nav) => (
+        <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
+          <nav.icon
+            className={`w-[24px] h-[24px] ${
+              path === nav.name ? "text-purple-800" : "text-white"
+            }`}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Navigation;
