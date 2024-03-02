622d56397b81a293d7c3d9911bb383b56a400ce5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-01 20:19:18 -0700

    Initialize Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20"></div>
+  );
+};
+
+export default Navbar;
