7ce626b196c52db8b9188f8b5d75ddee4c98e8ca
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-01 20:25:57 -0700

    Add horseLogo to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20"></div>
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
+      <div className="flex flex-row gap-3 items-center">
+        <Image
+          src="/horseLogo.jpg"
+          alt="logo"
+          width={40}
+          height={40}
+          className="w-full h-full object-contain rounded-full"
+        />
+      </div>
+    </div>
   );
 };
 
