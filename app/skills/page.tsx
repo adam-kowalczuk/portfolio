b8f80ac8027d5dab22d7f08b6e997f26ab44d1d3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-03 10:28:51 -0700

    Import Swiper

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,4 +1,9 @@
+"use skills";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { SkillData } from "@/constants";
+import Image from "next/image";
 
 const Page = () => {
   return <div>Skills</div>;
