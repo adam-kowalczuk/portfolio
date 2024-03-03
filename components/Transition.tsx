d7572642dcfa6e43a51de585e69ead71c3f9f2d4
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-02 20:55:54 -0700

    Import framer-motion, add TransitionVariants

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -1,4 +1,21 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+
+const TransitionVariants = {
+  initial: {
+    y: "100%",
+    height: "100%"
+  },
+  animate: {
+    y: "0%",
+    height: "0%"
+  },
+  exit: {
+    y: ["0%", "100%"],
+    height: ["0%", "100%"]
+  }
+};
 
 const Transition = () => {
   return <div>Transition</div>;
