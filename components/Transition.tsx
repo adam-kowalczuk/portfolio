8cbd92a93e47b73af0a0278ac8bb1e67644169d7
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-02 21:04:30 -0700

    Add motion.div for animated transition

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -18,7 +18,34 @@ const TransitionVariants = {
 };
 
 const Transition = () => {
-  return <div>Transition</div>;
+  return (
+    <div>
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[30] bg-[#2e2257]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[20] bg-[#3b2d71]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed right-0 h-screen w-screen bottom-full z-[20] bg-[#4b3792]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      />
+    </div>
+  );
 };
 
 export default Transition;
