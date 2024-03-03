ec500633dbe91635269169ca4487413a5b9fce25
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-03 10:32:37 -0700

    Add background image

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -4,9 +4,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { SkillData } from "@/constants";
 import Image from "next/image";
+import Autoplay from "swiper/modules";
 
 const Page = () => {
-  return <div>Skills</div>;
+  return (
+    <div
+      style={{ backgroundImage: "url(/bg-2.jpg)" }}
+      className="h-screen w-screen items-center justify-center bg-cover bg-center"
+    ></div>
+  );
 };
 
 export default Page;
