283fae44584b3b89c8e348bdf51c30a921ae951f
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-03 19:57:12 -0700

    Fix card padding, add useClient

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -11,12 +12,12 @@ const ProjectCard = ({ image, title, text }: Props) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
 
-  const handleFlip = () => {
+  function handleFlip() {
     if (!isAnimating) {
       setIsFlipped(!isFlipped);
       setIsAnimating(true);
     }
-  };
+  }
   return (
     <div
       onClick={handleFlip}
@@ -31,7 +32,7 @@ const ProjectCard = ({ image, title, text }: Props) => {
       >
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p4"
+          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
         >
           <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
           <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center">
@@ -40,7 +41,7 @@ const ProjectCard = ({ image, title, text }: Props) => {
         </div>
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p4"
+          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4"
         >
           <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
           <div className="flex flex-col gap-20 py-3 z-[30]">
