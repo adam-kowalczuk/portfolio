0a68e9a8b88e7108f717897e804e54b8d4b9927a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-03 19:25:55 -0700

    Define Props interface and handleFlip function

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+interface Props {
+  image: string;
+  title: string;
+  text: string;
+}
+
+const ProjectCard = ({ image, title, text }: Props) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleFlip = () => {
+    if (!isAnimating) {
+      setIsFlipped(!isFlipped);
+      setIsAnimating(true);
+    }
+  };
+  return (
+    <div
+      onClick={handleFlip}
+      className="w-[450px] h-[280px] rounded-md cursor-pointer"
+    >
+      ProjectCard
+    </div>
+  );
 };
 
 export default ProjectCard;
