0e575250280d3e125ad8125bec96ecca4626dbdd
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-04 19:00:12 -0700

    Add styling to contact card

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,14 @@ const Page = () => {
     <div
       style={{ backgroundImage: "url(/bg-3.jpg)" }}
       className="w-screen h-screen bg-cover bg-center flex items-center justify-center"
-    ></div>
+    >
+      <div
+        style={{ backgroundImage: "url(atombg-comp.webp)" }}
+        className="h-[60%] w-[80%] relative bg-cover bg-center rounded-xl border border-white "
+      >
+        <div className="absolute left-20 bottom-16 w-[70%] md:w-[30%]"></div>
+      </div>
+    </div>
   );
 };
 
