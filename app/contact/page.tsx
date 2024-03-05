3f099be0e8942fede2d714041556e019f73e77a5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-04 18:55:49 -0700

    Add background image

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      style={{ backgroundImage: "url(/bg-3.jpg)" }}
+      className="w-screen h-screen bg-cover bg-center flex items-center justify-center"
+    ></div>
+  );
 };
 
 export default Page;
