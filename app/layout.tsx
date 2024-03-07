fbcd5395d4b0b7f6fa8784654b8c75d9122ffadc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-07 14:09:52 -0700

    Align navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="{inter.className} flex flex-col items-center">
         <Navbar />
         {children}
         <Navigation />
