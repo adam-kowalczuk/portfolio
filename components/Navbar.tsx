aa0c8524081e5be52deb610df27ac26ff33a7e2d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-07 18:58:46 -0700

    Change text colour

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className="fixed top-0 z-[40] w-[80%] xl:w-[60%] h-[100px] bg-transparent flex justify-between items-center">
       {/* Navigation section */}
       <Link href="/">
-        <RxHome size={28} className="text-white hover:text-red-800" />
+        <RxHome size={28} className="text-gray-200 hover:text-red-800" />
       </Link>
       {/* Render Navigation links */}
       <Navigation />
