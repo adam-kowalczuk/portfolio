22b4cfd5f8edbafa97c78d54a9c3b4697f524aa0
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-10 16:34:39 -0600

    Remove background and change description

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -13,23 +13,14 @@ import { Autoplay } from "swiper/modules";
  **/
 const Page = () => {
   return (
-    <div
-      style={{ backgroundImage: "url(/bg-2.jpg)" }}
-      className="h-screen w-screen flex items-center justify-center bg-cover bg-center"
-    >
+    <div className="h-screen w-screen flex items-center justify-center bg-[#1b1d1e]">
       <div className="flex flex-col gap-20 max-w-[80%] text-center items-center">
         {/* Title and subtitle section */}
         <div className="flex flex-col items-center gap-4">
-          <h1 className="font-semibold text-white text-[50px]">
-            Skills{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
-              {" "}
-              &{" "}
-            </span>
-            Technologies
-          </h1>
-          <p className="text-gray-400 text-[20px]">
-            Using the latest tech this world has to offer
+          <h1 className="font-semibold text-gray-200 text-[48px]">Skills</h1>
+          <p className="text-gray-200">
+            This site was created using NextJS, TypeScript, and Tailwind CSS. It
+            is currently deployed via Vercel.
           </p>
         </div>
         {/* Swiper component for displaying skills */}
