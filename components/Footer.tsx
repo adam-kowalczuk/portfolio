63c269329ac38a4e3032ca61fd0a2a771c66836e
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-09 17:20:39 -0700

    Add source link to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="fixed bottom-0 z-[40] w-[80%] xl:w-[60%] h-[100px] bg-transparent flex justify-between items-center text-gray-200">
+      <Link
+        href="https://www.github.com/adam-kowalczuk/portfolio"
+        target="_blank"
+      >
+        Source
+      </Link>
+    </div>
+  );
 };
 
 export default Footer;
