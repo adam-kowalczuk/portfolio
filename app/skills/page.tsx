eb4939df052d0b66b3507ec7d8eadc109f38f44b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-12 17:35:55 -0600

    Change widths

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -14,9 +14,9 @@ import { Autoplay } from "swiper/modules";
 const Page = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-[#1b1d1e]">
-      <div className="flex flex-col gap-[50px] max-w-[80%] md:min-w-[60%] text-center items-center">
+      <div className="flex flex-col gap-[50px] max-w-[80%] xl:max-w-[60%] items-center mb-[5rem]">
         {/* Title and subtitle section */}
-        <div className="flex flex-col gap-4 min-w-[100%] text-center sm:text-left">
+        <div className="flex flex-col gap-4 w-[100%] text-center sm:text-left">
           <h1 className="font-semibold text-gray-200 text-[48px]">Skills</h1>
           <p className="text-gray-200">
             This site was created using NextJS, TypeScript, and Tailwind CSS. It
@@ -32,10 +32,7 @@ const Page = () => {
               alt={skill.name}
               width={skill.width}
               height={skill.height}
-              // className={`mb-4 h-[70px] ${
-              //   index === 5 || index === 11 ? "" : "mr-[6rem]"
-              // }`}
-              className="h-[80px] p-3 sm:p-2"
+              className="h-[75px] p-2"
             />
           ))}
         </div>
