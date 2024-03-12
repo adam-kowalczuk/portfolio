17591c3aeeee56973aa782c7b4af45015099acfe
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-12 15:10:21 -0600

    Change icon sizing

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -14,7 +14,7 @@ import { Autoplay } from "swiper/modules";
 const Page = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-[#1b1d1e]">
-      <div className="flex flex-col gap-[50px] max-w-[80%] md:max-w-[60%] text-center items-center">
+      <div className="flex flex-col gap-[50px] max-w-[80%] md:min-w-[60%] text-center items-center">
         {/* Title and subtitle section */}
         <div className="flex flex-col gap-4 min-w-[100%] text-center sm:text-left">
           <h1 className="font-semibold text-gray-200 text-[48px]">Skills</h1>
@@ -24,7 +24,7 @@ const Page = () => {
           </p>
         </div>
         {/* Swiper component for displaying skills */}
-        <div className="hidden sm:flex flex-row flex-wrap justify-left">
+        <div className="flex flex-row flex-wrap justify-center items-center">
           {SkillData.map((skill, index) => (
             <Image
               key={index}
@@ -32,9 +32,10 @@ const Page = () => {
               alt={skill.name}
               width={skill.width}
               height={skill.height}
-              className={`mb-4 h-[70px] ${
-                index === 5 || index === 11 ? "" : "mr-[6rem]"
-              }`}
+              // className={`mb-4 h-[70px] ${
+              //   index === 5 || index === 11 ? "" : "mr-[6rem]"
+              // }`}
+              className="h-[80px] p-3 sm:p-2"
             />
           ))}
         </div>
--- constants/index.ts
+++ constants/index.ts
@@ -4,75 +4,75 @@ export const SkillData = [
   {
     name: "Html 5",
     Image: "/html.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Css",
     Image: "/css.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "JavaScript",
     Image: "/js.png",
-    width: 70,
+    width: 80,
     height: 65
   },
   {
     name: "Tailwind Css",
     Image: "/tailwind.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "React",
     Image: "/react.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Redux",
     Image: "/redux.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
 
   {
     name: "TypeScript",
     Image: "/ts.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Next js 13",
     Image: "/next.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Framer Motion",
     Image: "/framer.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Stripe Payment",
     Image: "/stripe.webp",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Node js",
     Image: "/node-js.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   },
   {
     name: "Mongo db",
     Image: "/mongodb.png",
-    width: 70,
-    height: 70
+    width: 80,
+    height: 80
   }
 ];
 
