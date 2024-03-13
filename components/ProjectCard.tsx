e1efc07bb02724ca25ccf9cea2823ea3c1b392da
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-13 16:18:03 -0600

    Add Project links to backside of ProjectCard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { RxArrowTopRight } from "react-icons/rx";
 import { motion } from "framer-motion";
 
 // Defining a TypeScript interface for the props expected by the ProjectCard component
@@ -7,9 +8,10 @@ interface Props {
   image: string;
   title: string;
   text: string;
+  href: string;
 }
 
-const ProjectCard = ({ image, title, text }: Props) => {
+const ProjectCard = ({ image, title, text, href }: Props) => {
   // Using state to track whether the card is flipped and whether it's currently animating
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
@@ -39,7 +41,7 @@ const ProjectCard = ({ image, title, text }: Props) => {
         {/* Front side of the card */}
         <div
           style={{ backgroundImage: `url(${image})` }} // Setting background image
-          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4" // Applying styles using Tailwind CSS classes
+          className="w-full h-full group relative flip-card-front bg-cover bg-center text-gray-200 rounded-lg p-4" // Applying styles using Tailwind CSS classes
         >
           {/* Overlay for hover effect */}
           <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
@@ -50,15 +52,19 @@ const ProjectCard = ({ image, title, text }: Props) => {
         </div>
         {/* Back side of the card */}
         <div
-          style={{ backgroundImage: `url(${image})` }} // Setting background image
-          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4" // Applying styles using Tailwind CSS classes
+          className="w-full h-full group relative flip-card-back bg-[#1b1d1e] text-gray-200 rounded-lg p-4" // Applying styles using Tailwind CSS classes
         >
           {/* Overlay for hover effect */}
           <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
           {/* Project title and text */}
-          <div className="flex flex-col gap-20 py-3 z-[30]">
-            <h1 className="text-white text-2xl font-semibold">{title}</h1>
-            <p className="text-gray-200 text-[20px]">{text}</p>
+          <div className="flex flex-col gap-4 py-3 z-[30]">
+            <h1 className="flex flex-row items-center text-gray-200 text-2xl font-semibold">
+              {title}
+              <a href={href}>
+                <RxArrowTopRight className="hover:text-red-800 font-bold ml-2" />
+              </a>
+            </h1>
+            <p className="text-gray-200 text-[16px]">{text}</p>
           </div>
         </div>
       </motion.div>
