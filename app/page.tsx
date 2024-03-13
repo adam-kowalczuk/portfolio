b834382a3ac04b4486748c5a2eabb68bbe0b364f
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-13 15:39:32 -0600

    Change text alignment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="w-screen h-screen relative">
       <div className="flex flex-col justify-center items-center w-full h-full bg-[#1b1d1e]">
-        <div className="pb-[100px] flex flex-col gap-5 max-w-[80%] xl:max-w-[60%]">
+        <div className="pb-[100px] flex flex-col gap-5 max-w-[80%] xl:max-w-[60%] text-center sm:text-left">
           <h1 className="text-[48px] text-gray-200 font-semibold">Hello!</h1>
           <p className="text-gray-200 sm:block">
             I'm <span className="font-bold">Adam Kowalczuk</span>, a full-stack
