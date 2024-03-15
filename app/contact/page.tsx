9e667e9832ff7b7374a2696fe5231907dc00823b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-14 20:12:18 -0600

    Fix alignment on medium screens

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const Page = () => {
   return (
     <div className="w-screen h-screen bg-[#1b1d1e] flex items-center justify-center">
-      <div className="w-[80%] xl:w-[60%] flex flex-col items-center md:items-start">
+      <div className="w-[80%] xl:w-[60%] flex flex-col items-center sm:items-start">
         <div className="flex flex-col gap-4 w-[100%] text-center sm:text-left">
           <h1 className="font-semibold text-gray-200 text-[48px]">Contact</h1>
         </div>
-        <div className="w-[70%] md:w-[40%] mt-10">
+        <div className="w-[70%] sm:w-[40%] mt-10">
           <ContactForm />
         </div>
       </div>
