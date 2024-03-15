417bb487569b47bedf15426207d84e9dcec47ecc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-14 20:06:42 -0600

    Properly align ContactForm

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,16 +3,12 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-screen h-screen bg-[#1b1d1e] flex items-center justify-center ">
-      <div className="h-[60%] w-[80%] xl:w-[60%] relative rounded-lg border border-white ">
+    <div className="w-screen h-screen bg-[#1b1d1e] flex items-center justify-center">
+      <div className="w-[80%] xl:w-[60%] flex flex-col items-center md:items-start">
         <div className="flex flex-col gap-4 w-[100%] text-center sm:text-left">
           <h1 className="font-semibold text-gray-200 text-[48px]">Contact</h1>
-          <p className="text-gray-200">
-            This site was created using NextJS, TypeScript, and Tailwind CSS. It
-            is currently deployed via Vercel.
-          </p>
         </div>
-        <div className="absolute left-20 bottom-16 w-[70%] md:w-[30%]">
+        <div className="w-[70%] md:w-[40%] mt-10">
           <ContactForm />
         </div>
       </div>
