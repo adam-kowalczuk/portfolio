ed2dfd71644b417cc89e3de5d862fbf2a9d051ae
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-14 18:39:35 -0600

    Remove backgrounds and add heading/description

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,14 +3,15 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div
-      style={{ backgroundImage: "url(/bg-3.jpg)" }}
-      className="w-screen h-screen bg-cover bg-center flex items-center justify-center"
-    >
-      <div
-        style={{ backgroundImage: "url(atombg-comp.webp)" }}
-        className="h-[60%] w-[80%] relative bg-cover bg-center rounded-xl border border-white "
-      >
+    <div className="w-screen h-screen bg-[#1b1d1e] flex items-center justify-center ">
+      <div className="h-[60%] w-[80%] xl:w-[60%] relative rounded-lg border border-white ">
+        <div className="flex flex-col gap-4 w-[100%] text-center sm:text-left">
+          <h1 className="font-semibold text-gray-200 text-[48px]">Contact</h1>
+          <p className="text-gray-200">
+            This site was created using NextJS, TypeScript, and Tailwind CSS. It
+            is currently deployed via Vercel.
+          </p>
+        </div>
         <div className="absolute left-20 bottom-16 w-[70%] md:w-[30%]">
           <ContactForm />
         </div>
